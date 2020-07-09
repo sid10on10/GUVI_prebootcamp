af4956430712e32f442cfa4f189b6d07b40968c7
sid10on10 <siddhantk.singh.phe14@iitbhu.ac.in>
2020-07-09 13:03:21 +0530

    finding the culprits

---
+++ FindingtheCulprit/problem17/script.js
@@ -0,0 +1,4 @@
+let gifts = ["teddy bear", "drone", "doll"];
+for (i of gifts) {
+  console.log(`Wrapped ${i} and added a bow!`);
+}
---
+++ FindingtheCulprit/problem18/script.js
@@ -0,0 +1,9 @@
+let countdown = 100;
+while (countdown > 0) {
+
+  if(countdown == 0)
+  {
+   console.log("bomb triggered");
+  }
+  countdown--;
+}
